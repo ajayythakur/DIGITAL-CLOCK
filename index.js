19f719cea700d433422c48795e22d9c7031fadec
ajayythakur <ajay1270panwar@gmail.com>
2022-12-30 18:45:36 +0530

    DIGITAL CLOCK

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-let hrtime="";
+let hrtime;
 
 function timer(){
     let hour=document.getElementById("hrs");
@@ -10,7 +10,8 @@ function timer(){
     let hr= tym.getHours();
     // hr = (hr % 12) || 12;
 
-    hrtime=hr;  
+    hrtime=hr; 
+    // console.log(hrtime)
     let min=tym.getMinutes();
     let sec=tym.getSeconds();
     // console.log(hr,min,sec);
@@ -22,7 +23,7 @@ function timer(){
 
     let ampm=document.getElementById("AM_PM");
 
-if(hr>=12 && min>=0 && sec>=0){
+if(hr>=12){
     ampm.innerText="PM";
 
 }
@@ -33,32 +34,32 @@ else{
 }
 // let image=document.getElementById("image")
 let message=document.getElementById("sidebox");
-if(hr>=00 && hr<8 && min>=00 && sec>=0){
+if(hr>=00 && hr<8 ){
     message.innerText="IT'S TOO EARLY GO AND SLEEP AGAIN";
     document.getElementById("image").src = "./Component 30 – 1.svg";
     wishtext.style.fontSize="25px"
     wishtext.innerText="EARLY MORNING !!" 
 }
-else if(hr>=08 && hr<12 && min>=00 && sec>=0){
+else if(hr>=08 && hr<12 ){
     message.innerText="GRAB SOME HEALTHY BREAKFAST";
     document.getElementById("image").src = "./Component 30 – 1.svg";
     wishtext.style.fontSize="25px"
     wishtext.innerText="GOOD MORNING !! WAKE UP !!" 
 }
-else if(hr>=12 && hr<16 && min>=00 && sec>=0){
+else if(hr>=12 && hr<16 ){
     message.innerText="LET'S HAVE SOME LUNCH";
     document.getElementById("image").src = "./Group 5183.png";
     wishtext.style.fontSize="22px"
     wishtext.innerText="GOOD AFTERNOON !! TAKE SOME SLEEP";
 }
-else if(hr>=16 && hr<20 && min>=00 && sec>=0){
+else if(hr>=16 && hr<20 ){
     message.innerText="STOP YAWING, GET SOME TEA..ITS JUST EVENING";
     document.getElementById("image").src = "./lunch_image.png";
     wishtext.innerText="GOOD EVENING !!";
 
 
 }
-else if(hr>=20 && hr<=24 && min>=00 && sec>=0){
+else if(hr>=20 && hr<=24){
     message.innerText="close your eyes and to go sleep";
     document.getElementById("image").src = "./Group 5194.png";
     wishtext.innerText="GOOD NIGHT !!";
@@ -70,40 +71,71 @@ setInterval(function(){
 },1000);
 
 // Alarm Button
+let d=document.getElementById("dropdown");
+let d1=document.getElementById("d1");
+let d2=document.getElementById("d2");
+let d3=document.getElementById("d3");
+
+let dropdown;
+let dropdown2;
+let dropdown3;
+let dropdown4;
+d.addEventListener("change",function(event){
+const select=event.target;
+const value=select.value;
+ dropdown=select.selectedOptions[0].text;
+console.log(dropdown);
+
+})
+
+d1.addEventListener("change",function(event){
+    const select=event.target;
+    // const value=select.value;
+     dropdown2=select.selectedOptions[0].text;
+    console.log(dropdown2);
+})
+
+d2.addEventListener("change",function(event){
+    const select=event.target;
+    // const value=select.value;
+   dropdown3=select.selectedOptions[0].text;
+   console.log(dropdown3);
+})
+
+d3.addEventListener("change",function(event){
+    let select=event.target;
+    // let value=select.value;
+    dropdown4=select.selectedOptions[0].text;
+})
+
 
 let sa=document.getElementById("sa");
 sa.addEventListener("click",function(){
-
-    // let d=document.getElementById("dropdown").value;
-    // let d1=document.getElementById("d1").value;
-    // let d2=document.getElementById("d2").value;
-    // let d3=document.getElementById("d3").value; 
 
     let dvalue=document.getElementById("dropdown").value
     let dvalue1=document.getElementById("d1").value
     let dvalue2=document.getElementById("d2").value
     let dvalue3=document.getElementById("d3").value
 
-    let wake=document.getElementById("wktxt");
-    wake.innerText= dvalue;
-    let lunch=document.getElementById("ltxt");
-    lunch.innerText=dvalue1;
-    let nap=document.getElementById("ntxt");
-    nap.innerText=dvalue2;
-    let night=document.getElementById("nightxt");
-    night.innerText=dvalue3;
-   
 
     let message=document.getElementById("sidebox");
-    let wishtext1=document.getElementById("wishtext")
+    let wishtext1=document.getElementById("wishtext");
 
 
+    let wake=document.getElementById("wktxt");
+    wake.innerText= dropdown;
+    let lunch=document.getElementById("ltxt");
+    lunch.innerText=dropdown2;
+    let nap=document.getElementById("ntxt");
+    nap.innerText=dropdown3;
+    let night=document.getElementById("nightxt");
+    night.innerText=dropdown4;
+   
 
 
 
     // functioning - if dropdown and live time matches
     // function imgtxtChng(){
-
 
         if(dvalue==hrtime){
             console.log(dvalue)
